9576f9bf596fe7c217849f6d83c09cf581b70e87
jiyuu <lucas.gabriel9595@gmail.com>
2026-06-22 17:43:28 -0300

    Adicionada a verificação do YANDEX

---
+++ src/assets/scripts/ferramentas/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC3S_v3XTrwKrAAQyHt4DFZ1Hz3LdLoUfk",
+  authDomain: "elobetter9595.firebaseapp.com",
+  projectId: "elobetter9595",
+  storageBucket: "elobetter9595.firebasestorage.app",
+  messagingSenderId: "381856521042",
+  appId: "1:381856521042:web:5f638166ecefdcb854b88b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
